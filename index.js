66ab8b16a494c9f566add3084550ac4f8a487c12
Victoria Laputko <vikalaputko87@gmail.com>
2023-08-07 19:29:34 +0300

    test

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const ladder = {
-    step: 0,
-    up: function () {
-        this.step++;
-        return this;
-    },
-    down: function () {
-        this.step--;
-        return this;
-    },
-    showStep: function () {
-        alert(this.step);
-        return this;
-    }
+  step: 0,
+  up: function () {
+      this.step++;
+      return this;
+  },
+  down: function () {
+      this.step--;
+      return this;
+  },
+  showStep: function () {
+      alert(this.step);
+      return this;
+  }
 };
 
 ladder.up().up().down().showStep(); // 1
+ тест
